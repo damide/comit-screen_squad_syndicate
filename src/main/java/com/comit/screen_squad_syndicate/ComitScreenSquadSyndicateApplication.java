4e98c31c47ab912aa0388fed779ecba62e6dd181
Daidee <monsuratsaliu14@gmail.com>
2023-04-13 02:16:58 -0600

    first commit

---
+++ src/main/java/com/comit/screen_squad_syndicate/ComitScreenSquadSyndicateApplication.java
@@ -0,0 +1,13 @@
+package com.comit.screen_squad_syndicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitScreenSquadSyndicateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitScreenSquadSyndicateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comit/screen_squad_syndicate/ComitScreenSquadSyndicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.screen_squad_syndicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComitScreenSquadSyndicateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
