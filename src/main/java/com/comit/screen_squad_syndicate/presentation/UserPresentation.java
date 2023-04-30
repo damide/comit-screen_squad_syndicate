8729519b4a483df0adb6e9e057a68b5cc903be0a
Daidee <monsuratsaliu14@gmail.com>
2023-04-30 16:27:33 -0600

    add fragment

--- src/main/java/com/comit/screen_squad_syndicate/presentation/UserPresentation.java
+++ src/main/java/com/comit/screen_squad_syndicate/presentation/UserPresentation.java
@@ -1,8 +1,16 @@
 package com.comit.screen_squad_syndicate.presentation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserPresentation {
+	
+	@GetMapping("/")
+	public String getAllMovies() {
+		
+		return "index";
+		
+	}
 
 }
